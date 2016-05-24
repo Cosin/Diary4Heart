f7d4582fa1fb8b063c1807944ee4397c546a44b5
Sh1nes00n <sh1nes00n@live.com>
2016-05-24 18:02:31 +0800

    添加菜单

--- app/src/main/java/sx/xss/diary4heart/MainActivity.java
+++ app/src/main/java/sx/xss/diary4heart/MainActivity.java
@@ -3,6 +3,7 @@ package sx.xss.diary4heart;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,17 +15,28 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv)
     TextView tv;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        tv.setText("333333333333333");
-        initToolBar(toolbar, "33333");
+
+        initToolBar(toolbar, "觅心日记");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 
     public void initToolBar(Toolbar toolbar, String title) {
         toolbar.setTitle(title);
+        toolbar.setLogo(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
     }
 }
