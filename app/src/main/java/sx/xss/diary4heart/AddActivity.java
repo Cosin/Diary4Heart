a16ca58193c53e60505d1b095cd06570a6844c75
Sh1nes00n <sh1nes00n@live.com>
2016-05-29 23:19:00 +0800

    添加文章完善

--- app/src/main/java/sx/xss/diary4heart/AddActivity.java
+++ app/src/main/java/sx/xss/diary4heart/AddActivity.java
@@ -3,10 +3,14 @@ package sx.xss.diary4heart;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import dog.debug.hmlite.HMLite;
 
 /**
  * Created by Hm on 2016/5/27.
@@ -16,6 +20,12 @@ public class AddActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.submit)
+    TextView submit;
+    @BindView(R.id.content_tv)
+    AppCompatEditText content_tv;
+    @BindView(R.id.emotion_tv)
+    AppCompatEditText emotionTv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,12 +33,29 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         ButterKnife.bind(this);
 
-        initToolBar(toolbar,"添加");
+        initToolBar(toolbar, "添加", false);
     }
 
-    public void initToolBar(Toolbar toolbar, String title) {
+    /**
+     * 初始化Toolbar
+     *
+     * @param toolbar
+     * @param title
+     * @param has_icon
+     */
+    public void initToolBar(Toolbar toolbar, String title, boolean has_icon) {
         toolbar.setTitle(title);
-        toolbar.setLogo(R.mipmap.ic_launcher);
+        if (has_icon)
+            toolbar.setLogo(R.mipmap.heart);
         setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.submit)
+    public void onClick() {
+        String content = content_tv.getText().toString();
+        String emotion = emotionTv.getText().toString();
+        HMLite hm = new HMLite(this, "diary4heart.db", R.raw.diary4heart);
+        hm.init();
+        hm.insert("insert into d4h_content() values(%s,%s)",new String[]{content,emotion});
     }
 }
---
+++ app/src/main/java/sx/xss/utils/Tools.java
@@ -0,0 +1,14 @@
+package sx.xss.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import sx.xss.diary4heart.R;
+
+/**
+ * Created by Hm on 2016/5/29.
+ */
+
+public class Tools {
+
+
+}
